9f2c254b1e6bd8faf63e7cd202786d489accba0b
Jarvis <13g7895123@gmail.com>
2023-09-05 00:02:54 +0800

    [edit] test api

--- api-ori/api/bonus-register/admin/login.php
+++ api-ori/api/bonus-register/admin/login.php
@@ -3,8 +3,6 @@ include_once(__DIR__ . '/../../../__Class/ClassLoad.php');
 include_once('/../config.php');
 include_once('/../tools.php');
 
-echo '123';
-
 if (isset($_GET['action'])){
     switch($_GET['action']){
         case 'login':
