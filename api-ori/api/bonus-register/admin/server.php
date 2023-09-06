70cae6a91c1cfb0fb0d3325dc127efd74e493781
Jarvis <13g7895123@gmail.com>
2023-09-06 21:59:32 +0800

    [edit] server api

---
+++ api-ori/api/bonus-register/admin/server.php
@@ -0,0 +1,32 @@
+<?php
+/* ===== test url =====
+ *http://170.187.229.132:9091/api/bonus-register/admin/system_admin.php?action=system_admin
+ * ===============*/
+
+include_once(__DIR__ . '/../../../__Class/ClassLoad.php');
+include_once(__DIR__ . '/../config.php');
+include_once(__DIR__ . '/../tools.php');
+
+if (isset($_GET['action'])){
+    switch($_GET['action']){
+        case 'server':
+            // 取得 POST DATA
+            $json_data = file_get_contents('php://input');  // string
+            $post_data = json_decode($json_data, true);     // string轉array
+
+            MYPDO::$table = 'server';
+            $results = MYPDO::select();
+
+            if (empty($results)){
+                $return['success'] = false;
+                $return['msg'] = '查無資料';
+            }else{
+                $return['success'] = true;
+                $return['data'] = $results;
+            }
+
+            echo json_encode($return);
+            break;
+    }
+}
+?>
