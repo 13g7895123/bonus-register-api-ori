a1ce235564601165304548447b70798e6e1c2437
Jarvis <13g7895123@gmail.com>
2023-09-06 21:01:17 +0800

    [add] revise action name

--- api-ori/api/bonus-register/admin/system_user.php
+++ api-ori/api/bonus-register/admin/system_user.php
@@ -9,7 +9,7 @@ include_once(__DIR__ . '/../tools.php');
 
 if (isset($_GET['action'])){
     switch($_GET['action']){
-        case 'player_user':
+        case 'system_user':
             // 取得 POST DATA
             $json_data = file_get_contents('php://input');  // string
             $post_data = json_decode($json_data, true);     // string轉array
