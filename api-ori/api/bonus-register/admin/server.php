0196821c1e0a3ad293acddecb2e14edfaa575242
Jarvis <13g7895123@gmail.com>
2023-09-12 23:02:40 +0800

    [edit] revise api

--- api-ori/api/bonus-register/admin/server.php
+++ api-ori/api/bonus-register/admin/server.php
@@ -27,6 +27,53 @@ if (isset($_GET['action'])){
 
             echo json_encode($return);
             break;
+        case 'get_server':     // 透過ID查詢資料
+
+            $post_data = tools::post_data();    // 取得 POST DATA
+
+            MYPDO::$table = 'server';
+            MYPDO::$where = ['id' => $post_data['id']];
+            $result = MYPDO::first();
+
+            $return['test'] = $post_data;
+            if (empty($result)){
+                $return['success'] = false;
+                $return['msg'] = '查無資料';
+            }else{
+                $return['success'] = true;
+                $return['data'] = $result;
+            }
+
+            echo json_encode($return);
+            break;
+        case 'add_server':
+            $post_data = tools::post_data();    // 取得 POST DATA
+
+            MYPDO::$table = 'server';
+            MYPDO::$data = [
+                'name' => $post_data['name'],
+                'code_name' => $post_data['code_name'],
+                'max_num' => $post_data['max_num'],
+                'db_name' => $post_data['db_name'],
+                'db_ip' => $post_data['db_ip'],
+                'db_port' => $post_data['db_port'],
+                'db_username' => $post_data['db_username'],
+                'db_password' => $post_data['db_password'],
+                'switch' => $post_data['switch'],
+            ];
+            $insert_id = MYPDO::insert();
+
+            if ($insert_id > 0){
+                $return['success'] = 'true';
+                $return['msg'] = '新增資料成功';
+                $return['insert_id'] = $insert_id;
+            }else{
+                $return['success'] = 'true';
+                $return['msg'] = '寫入資料庫錯誤';
+            }
+
+            echo json_encode($return);
+            break;
     }
 }
 ?>
