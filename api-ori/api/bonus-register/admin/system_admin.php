6a9be0a44d7fc426fbceb77a264c2384860e002b
Jarvis <13g7895123@gmail.com>
2023-09-12 21:23:41 +0800

    [edit] revise api

--- api-ori/api/bonus-register/admin/system_admin.php
+++ api-ori/api/bonus-register/admin/system_admin.php
@@ -27,6 +27,25 @@ if (isset($_GET['action'])){
 
             echo json_encode($return);
             break;
+        case 'get_system_admin':     // 透過ID查詢資料
+
+            $post_data = tools::post_data();    // 取得 POST DATA
+
+            MYPDO::$table = 'system_admin';
+            MYPDO::$where = ['id' => $post_data['id']];
+            $result = MYPDO::first();
+
+            $return['test'] = $post_data;
+            if (empty($result)){
+                $return['success'] = false;
+                $return['msg'] = '查無資料';
+            }else{
+                $return['success'] = true;
+                $return['data'] = $result;
+            }
+
+            echo json_encode($return);
+            break;
         case 'add_system_admin':
             $post_data = tools::post_data();    // 取得 POST DATA
 
