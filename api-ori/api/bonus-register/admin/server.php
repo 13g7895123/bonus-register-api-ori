2729a9a4201b0b976947949b4b55e2afd2d8e9bd
Jarvis <13g7895123@gmail.com>
2023-09-15 22:42:06 +0800

    [edit] test

--- api-ori/api/bonus-register/admin/server.php
+++ api-ori/api/bonus-register/admin/server.php
@@ -119,18 +119,20 @@ if (isset($_GET['action'])){
             echo json_encode($return);
             break;
         case 'bg_img_upload':
-            if ($_FILES['my_file']['error'] === UPLOAD_ERR_OK){
-                echo '檔案名稱: ' . $_FILES['my_file']['name'] . '<br/>';
-                echo '檔案類型: ' . $_FILES['my_file']['type'] . '<br/>';
-                echo '檔案大小: ' . ($_FILES['my_file']['size'] / 1024) . ' KB<br/>';
-                echo '暫存名稱: ' . $_FILES['my_file']['tmp_name'] . '<br/>';
+            // $file_info = $_FILES['file'];
+            // echo json_encode($file_info);
+            if ($_FILES['file']['error'] === UPLOAD_ERR_OK){
+                echo '檔案名稱: ' . $_FILES['file']['name'] . '<br/>';
+                echo '檔案類型: ' . $_FILES['file']['type'] . '<br/>';
+                echo '檔案大小: ' . ($_FILES['file']['size'] / 1024) . ' KB<br/>';
+                echo '暫存名稱: ' . $_FILES['file']['tmp_name'] . '<br/>';
               
                 # 檢查檔案是否已經存在
-                if (file_exists('upload/' . $_FILES['my_file']['name'])){
+                if (file_exists('upload/' . $_FILES['file']['name'])){
                   echo '檔案已存在。<br/>';
                 } else {
-                    $file = $_FILES['my_file']['tmp_name'];
-                    $dest = 'upload/' . $_FILES['my_file']['name'];
+                    $file = $_FILES['file']['tmp_name'];
+                    $dest = 'upload/' . $_FILES['file']['name'];
               
                     if ( !file_exists($dest) ){
                         mkdir($dest,0775,true);
@@ -138,9 +140,9 @@ if (isset($_GET['action'])){
                     # 將檔案移至指定位置
                     move_uploaded_file($file, $dest);
                 }
-              } else {
-                echo '錯誤代碼：' . $_FILES['my_file']['error'] . '<br/>';
-              }
+            } else {
+            echo '錯誤代碼：' . $_FILES['file']['error'] . '<br/>';
+            }
             break;
     }
 }
