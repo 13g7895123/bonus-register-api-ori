9a770ceb33f19af3c545a56119a2396b5c5c5811
Jarvis <13g7895123@gmail.com>
2023-10-09 09:57:04 +0800

    test

--- api-ori/api/bonus-register/phone.php
+++ api-ori/api/bonus-register/phone.php
@@ -2,7 +2,7 @@
 include_once(__DIR__ . '/../__Class/ClassLoad.php');
 include_once('./config.php');
 include_once('./tools.php');
-include('./common_class.php');
+include_once('./common_class.php');
 
 // 引入DB資訊
 common::db_config();
